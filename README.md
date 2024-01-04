b15090a939855c04d71a7c38f777364695a7a47e
CodingManoj <CodingManoj@gmail.com>
2024-01-04 07:26:13 +0530

    TF VPC Auto Push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tf-module-docdb
+
+This is the backend module to terraform-databases.
+
+All the code that's needed to provision document-db will be placed in this particular repository
---
+++ docdb.tf
@@ -0,0 +1,22 @@
+# Provisions Document DB Cluster
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "roboshop-${var.ENV}-docdb"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+#   backup_retention_period = 5                             # Commented to avoid backups in lab
+#   preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  db_subnet_group_name    = ?
+}
+
+
+# Creates a subnet-groups
+resource "aws_docdb_subnet_group" "default" {
+  name       = "roboshop-${var.ENV}-docdb-subnet-group"
+  subnet_ids = ???
+
+  tags = {
+    Name = "roboshop-${var.ENV}-docdb-subnet-group"
+  }
+}
