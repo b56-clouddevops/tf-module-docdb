8c8de7a2d2e97460fb026c11865fd83a795d1897
CodingManoj <mvp2612@hotmail.com>
2024-01-09 19:54:34 -0500

    AutoPush

---
+++ schema.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "schema" {
+
+    # This make sures that this null_resource will only be executed post the creation of the RDS only    
+    depends_on = [aws_db_instance.docdb]
+
+      provisioner "local-exec" {
+        command = <<EOF
+            cd /tmp
+            wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+            unzip -o mongodb.zip
+            cd mongodb-main
+            ls -ltr
+            mongo --ssl --host roboshop-dev-docdb.cluster-c93whrwsdihk.us-east-1.docdb.amazonaws.com:27017 --sslCAFile global-bundle.pem --username admin1 --RoboShop1 < catalogue.js
+            mongo --ssl --host roboshop-dev-docdb.cluster-c93whrwsdihk.us-east-1.docdb.amazonaws.com:27017 --sslCAFile global-bundle.pem --username admin1 --RoboShop1 < users.js
+        EOF
+    }
+}
