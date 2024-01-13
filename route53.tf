e62cdf411ba4a5c738df3c07ca5bfc3880b93a01
CodingManoj <CodingManoj@gmail.com>
2024-01-13 22:40:01 +0530

    TF DB Auto Push

--- route53.tf
+++ route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "docdb_dns_record" {
   name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
-  records = [aws_db_instance.docdb.endpoint]
+  records = [aws_docdb_cluster.docdb.endpoint]
 }
